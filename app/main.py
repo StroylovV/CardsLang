80d46422546a0f6538df40d5fa65f22e6a9596da
StroylovV <spartan9611@gmail.com>
2026-06-15 22:12:56 +0600

    CORS Bugfix

--- app/main.py
+++ app/main.py
@@ -4,21 +4,16 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend
-from redis import asyncio as aioredis
+from fastapi_cache.backends.inmemory import InMemoryBackend
 import uvicorn
 
 from app.api import api_router
-
 from app.core.config import cors_config
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncIterator[None]:
     
-    
-    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
-    FastAPICache.init(RedisBackend(redis), prefix="cache")
-    
+    FastAPICache.init(InMemoryBackend(), prefix="cache")
     yield
     
 app = FastAPI(title="Words API", lifespan=lifespan)
@@ -27,11 +22,12 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
-    
     allow_origins=[
         "https://cards-lang.vercel.app", 
         "http://localhost:3000"          
     ],
+    
+    allow_origin_regex=r"https://.*\.vercel\.app", 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
